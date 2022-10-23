99bca9d199244efa64f0065312087e35706cb2a2
Tania Shershen <shersshen.tania@gmail.com>
2022-10-23 12:24:17 +0300

    Uploading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import NoteList from "./components/NotesList";
 import SearchBar from "./components/SearchBar";
@@ -13,6 +13,23 @@ const App = () => {
   const [searchText, setSearchText] = useState("");
 
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+		const savedNotes = JSON.parse(
+			localStorage.getItem('react-notes-app-data')
+		);
+
+		if (savedNotes) {
+			setNotes(savedNotes);
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem(
+			'react-notes-app-data',
+			JSON.stringify(notes)
+		);
+	}, [notes]);
 
   const addNote = (text) => {
     const date = new Date();
